5a56ec0abe52acb41daaf447b5fadd4e32e261ac
TomWulf <Tom.Wulf@uc.edu>
2022-01-10 12:14:02 -0500

    Added another msg to Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello World");
 
         System.out.println("Welcome back to Java");
+
+        int favNum = SafeInput.getRangedInt(in, "Enter your fav num", 1, 10);
+
+        System.out.println("Fav num is " + favNum);
 
     }
 }
